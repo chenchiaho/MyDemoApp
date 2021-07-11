1e0b8772951b0da2aaaa76f7aca7183381bc17d7
Kevin La <chenchiaho@gmail.com>
2021-07-12 06:34:25 +0800

    fix databinding and display issue

--- app/src/main/java/com/example/android/mydemoapp/ui/weather/future/DetailFragment.kt
+++ app/src/main/java/com/example/android/mydemoapp/ui/weather/future/DetailFragment.kt
@@ -9,26 +9,25 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.android.mydemoapp.R
 import com.example.android.mydemoapp.databinding.FutureDetailFragmentBinding
-import com.example.android.mydemoapp.databinding.FutureListFragmentBinding
-
-class FutureDetailFragment : Fragment() {
 
 
-    private lateinit var viewModel: FutureDetailViewModel
+class DetailFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         val binding: FutureDetailFragmentBinding = DataBindingUtil.inflate(
-            inflater,
-            R.layout.future_detail_fragment,
-            container, false)
+                inflater,
+                R.layout.future_detail_fragment,
+                container,false
+        )
 
-        binding.lifecycleOwner = this
+        binding.lifecycleOwner = viewLifecycleOwner
 
-        val weather = FutureDetailFragmentArgs.fromBundle(requireArguments()).selectedWeather
+        val weather = DetailFragmentArgs.fromBundle(requireArguments()).selectedWeather
         binding.weatherDetail = weather
 
         return binding.root
