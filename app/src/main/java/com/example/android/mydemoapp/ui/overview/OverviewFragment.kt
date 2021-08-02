bb564a2c9e5063a56b3e5bea53c3b18c62e07a2f
Kevin La <chenchiaho@gmail.com>
2021-08-02 15:20:10 +0800

    set button enable and disable

--- app/src/main/java/com/example/android/mydemoapp/ui/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/mydemoapp/ui/overview/OverviewFragment.kt
@@ -1,11 +1,14 @@
 package com.example.android.mydemoapp.ui.overview
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -38,9 +41,20 @@ class OverviewFragment : Fragment() {
         val enterButton = binding.buttonEnter
         val editText = binding.editTextImpression
 
-        if (editText.equals("")) {
-            enterButton.isEnabled = false
-        } else true
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val editTextInput = editText.text.toString().trim()
+
+                enterButton.isEnabled = editTextInput.isNotEmpty()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
 
 
         viewModel.repository.editImpression.observe(viewLifecycleOwner, Observer {
